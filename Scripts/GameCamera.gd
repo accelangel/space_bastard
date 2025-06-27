ab4f8ebf30593860f6d7de28f62a0bf375fc18eb
accelangel <accelangel@users.noreply.github.com>
2025-06-27 18:31:01 -0400

    added basic WASD panning

--- Scripts/GameCamera.gd
+++ Scripts/GameCamera.gd
@@ -1,22 +1,39 @@
 extends Camera2D
 
+@export var zoomSpeed : float = 10
+
+var zoomTarget :Vector2
+
+
 func _ready():
+	zoomTarget = zoom
 	pass
 
 func _process(delta):
-	Zoom()
+	Zoom(delta)
 	Pan()
 	ClickAndDrag()
 
-func Zoom():
+func Zoom(delta):
 	if Input.is_action_just_pressed("camera_zoom_in"):
-		zoom = zoom * 1.1
+		zoomTarget *= 1.1
 	
 	if Input.is_action_just_pressed("camera_zoom_out"):
-		zoom = zoom * 0.9
+		zoomTarget *= 0.9
+		
+	zoom = zoom.slerp(zoomTarget, zoomSpeed * delta) # Makes the zoom choppy and more sluuuurpy aw bby
 	pass
 
 func Pan():
+	if Input.is_action_pressed("camera_move_right"):
+		position.x += 1
+	if Input.is_action_pressed("camera_move_left"):
+		position.x -= 1
+	if Input.is_action_pressed("camera_move_up"):
+		position.y -= 1
+	if Input.is_action_pressed("camera_move_down"):
+		position.y += 1
+	
 	pass
 
 func ClickAndDrag():
