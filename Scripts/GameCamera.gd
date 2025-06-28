15698741e2f514e84206d9a926c78b86bff14dbc
accelangel <accelangel@users.noreply.github.com>
2025-06-28 01:55:55 -0400

    Added to cursor zooming, cleaned up the script

--- Scripts/GameCamera.gd
+++ Scripts/GameCamera.gd
@@ -1,15 +1,24 @@
 extends Camera2D
 
-@export var zoomSpeed : float = 10
-var zoomTarget :Vector2
+# Map configuration
+var map_size = Vector2(131072, 131072)
+var zoom_min: Vector2 = Vector2(0.0146484375, 0.0146484375)
+var zoom_max: Vector2 = Vector2(5, 5)
 
+# Zoom system variables
+var zoom_start_mouse_pos = Vector2.ZERO
+var zoom_start_screen_pos = Vector2.ZERO
+@export var zoomSpeed: float = 11
+var zoomTarget: Vector2
+
+# Click and drag variables
 var dragStartMousePos = Vector2.ZERO
 var dragStartCameraPos = Vector2.ZERO
-var isDragging : bool = false
+var isDragging: bool = false
 
 func _ready():
 	zoomTarget = zoom
-	pass
+	zoom_min = calculate_min_zoom()
 
 func _process(delta):
 	Zoom(delta)
@@ -17,16 +26,33 @@ func _process(delta):
 	ClickAndDrag()
 
 func Zoom(delta):
+	var scroll = 0
+
 	if Input.is_action_just_pressed("camera_zoom_in"):
-		zoomTarget *= 1.1
-	
-	if Input.is_action_just_pressed("camera_zoom_out"):
-		zoomTarget *= 0.9
+		scroll = 1
+	elif Input.is_action_just_pressed("camera_zoom_out"):
+		scroll = -1
+
+	if scroll != 0:
+		var zoom_factor = 1.1 ** scroll
+		zoom_start_mouse_pos = get_global_mouse_position()
+		zoom_start_screen_pos = Vector2(get_viewport().get_mouse_position())
+		zoomTarget *= zoom_factor
+		zoomTarget = clamp(zoomTarget, zoom_min, zoom_max)
+
+	# Smooth interpolation toward target zoom
+	if zoom.distance_to(zoomTarget) > 0.001:
+		var mouse_world_before = zoom_start_mouse_pos
+		zoom = zoom.slerp(zoomTarget, zoomSpeed * delta)
 		
-	zoom = zoom.slerp(zoomTarget, zoomSpeed * delta) # Makes the zoom choppy and more sluuuurpy aw bby
+		var viewport_center = Vector2(get_viewport().size) / 2
+		var mouse_world_after = position + (zoom_start_screen_pos - viewport_center) / zoom
+		
+		position += mouse_world_before - mouse_world_after
 
 func Pan(delta):
 	var moveAmount = Vector2.ZERO
+
 	if Input.is_action_pressed("camera_move_right"):
 		moveAmount.x += 1
 	if Input.is_action_pressed("camera_move_left"):
@@ -35,18 +61,24 @@ func Pan(delta):
 		moveAmount.y -= 1
 	if Input.is_action_pressed("camera_move_down"):
 		moveAmount.y += 1
-	
+
 	moveAmount = moveAmount.normalized()
-	position += moveAmount * delta * 1000 * (1/zoom.x)
+	position += moveAmount * delta * 1000 * (1 / zoom.x)
 
 func ClickAndDrag():
 	if !isDragging and Input.is_action_just_pressed("camera_pan"):
 		dragStartMousePos = get_viewport().get_mouse_position()
 		dragStartCameraPos = position
 		isDragging = true
+
 	if isDragging and Input.is_action_just_released("camera_pan"):
 		isDragging = false
-		
+
 	if isDragging:
 		var moveVector = get_viewport().get_mouse_position() - dragStartMousePos
-		position = dragStartCameraPos - moveVector * (1/zoom.x)
+		position = dragStartCameraPos - moveVector * (1 / zoom.x)
+
+func calculate_min_zoom():
+	var viewport_size = get_viewport_rect().size
+	var required_zoom = (viewport_size.x / map_size.x) * 0.9995
+	return Vector2(required_zoom, required_zoom)
